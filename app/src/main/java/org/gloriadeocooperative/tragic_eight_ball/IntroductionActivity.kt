485f594649be6b561f117eb894ea3efdf9f1a468
dsh <dsh@dman13.dyndns.org>
2022-02-07 18:25:40 -0500

    Rename MainActivity to IntroductionActivity

---
+++ app/src/main/java/org/gloriadeocooperative/tragic_eight_ball/IntroductionActivity.kt
@@ -0,0 +1,13 @@
+package org.gloriadeocooperative.tragic_eight_ball
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IntroductionActivity : AppCompatActivity()
+    {
+    override fun onCreate(savedInstanceState: Bundle?)
+        {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_introduction)
+        }
+    }
