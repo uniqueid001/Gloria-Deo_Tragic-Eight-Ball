8f96e7f76df12b77667479d5111292b5475dba68
dsh <dsh@dman13.dyndns.org>
2022-02-18 21:24:58 -0500

    Added some comments to explain the use of the ShakeDetector

--- app/src/main/kotlin/org/gloriadeocooperative/tragic_eight_ball/IntroductionActivity.kt
+++ app/src/main/kotlin/org/gloriadeocooperative/tragic_eight_ball/IntroductionActivity.kt
@@ -4,22 +4,23 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-
 class IntroductionActivity : AppCompatActivity()
     {
-    private lateinit var mShakeDetector: ShakeDetector
+    /**
+     * A "hard" reference to the ShakeDetector instance, so that it will not be
+     * garbage-collected too early.
+     */
+    private lateinit var shakeDetector: ShakeDetector
 
     override fun onCreate(savedInstanceState: Bundle?)
         {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_introduction)
 
-        // ShakeDetector initialization
-        this.mShakeDetector = ShakeDetector(this, this, this::handleShakeEvent)
+        // Create a demonstration/proof-of-concept ShakeDetector that simply
+        // displays when a shake was detected.
+        this.shakeDetector = ShakeDetector(this, this,
+                    { count -> Toast.makeText(this, "Shake detected ${count}", Toast.LENGTH_SHORT).show() } )
         } // onCreate()
 
-    fun handleShakeEvent(count: Int)
-        {
-        Toast.makeText(this, "Shake detected ${count}", Toast.LENGTH_SHORT).show()
-        }
     } // class
