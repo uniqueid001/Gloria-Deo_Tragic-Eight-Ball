d406bc027698d92c5a7aba9802c11767a5d37747
dsh <dsh@dman13.dyndns.org>
2022-03-14 16:35:32 -0400

    Created UserAnswersActivity

---
+++ app/src/main/java/org/gloriadeocooperative/tragic_eight_ball/UserAnswersActivity.kt
@@ -0,0 +1,13 @@
+package org.gloriadeocooperative.tragic_eight_ball
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserAnswersActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_answers)
+    }
+}
