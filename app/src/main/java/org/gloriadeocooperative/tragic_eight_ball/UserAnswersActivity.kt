96d477e2c7438f9f05746d2b1ca6e495cbdb6551
dsh <dsh@dman13.dyndns.org>
2022-03-22 22:34:29 -0400

    Use ViewBinding in UserAnswersActivity

--- app/src/main/java/org/gloriadeocooperative/tragic_eight_ball/UserAnswersActivity.kt
+++ app/src/main/java/org/gloriadeocooperative/tragic_eight_ball/UserAnswersActivity.kt
@@ -2,12 +2,17 @@ package org.gloriadeocooperative.tragic_eight_ball
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.gloriadeocooperative.tragic_eight_ball.databinding.ActivityUserAnswersBinding
 
 class UserAnswersActivity : AppCompatActivity()
 {
+    private lateinit var viewBinding: ActivityUserAnswersBinding
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_answers)
+        this.viewBinding = ActivityUserAnswersBinding.inflate(this.layoutInflater)
+        setContentView(this.viewBinding.root)
     }
+
 }
