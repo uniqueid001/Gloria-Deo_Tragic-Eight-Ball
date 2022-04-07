732e6374aa47ef24d034d9e16e71eec627c797be
dsh <dsh@dman13.dyndns.org>
2022-04-06 22:36:46 -0400

    Add Henry's work-in-progress for the "thinking" activity
    (this does replace the temporary, placeholder, "thinking" activity which
    Zach had created to verify using Intents to navigate)

--- app/src/main/java/org/gloriadeocooperative/tragic_eight_ball/ThinkingActivity.kt
+++ app/src/main/java/org/gloriadeocooperative/tragic_eight_ball/ThinkingActivity.kt
@@ -1,22 +1,40 @@
 package org.gloriadeocooperative.tragic_eight_ball
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.gloriadeocooperative.tragic_eight_ball.databinding.ActivityAskQuestion2Binding
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import org.gloriadeocooperative.tragic_eight_ball.databinding.ActivityThinkingBinding
 
-class ThinkingActivity() : AppCompatActivity() {
+class ThinkingActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityThinkingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding= ActivityThinkingBinding.inflate(layoutInflater)
-
+        binding = ActivityThinkingBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val intent = intent
-        val theQuestion =intent.getStringExtra("question")
-binding.theQuestionTextView.text = theQuestion
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_thinking)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_thinking)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
