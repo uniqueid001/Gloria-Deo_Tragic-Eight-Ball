4943d5821acb0ea6972a11dff49b21e65a2b4bf6
dsh <dsh@dman13.dyndns.org>
2022-05-03 19:34:43 -0400

    Zach's updates to AskQuestion2 activity
    * add a menu and toolbar to the activity
    * limit the length of a user's question
    * do not proceed when a shake is detected but the user did not input a question

--- app/src/main/java/org/gloriadeocooperative/tragic_eight_ball/AskQuestion2.kt
+++ app/src/main/java/org/gloriadeocooperative/tragic_eight_ball/AskQuestion2.kt
@@ -3,6 +3,9 @@ package org.gloriadeocooperative.tragic_eight_ball
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import org.gloriadeocooperative.tragic_eight_ball.databinding.ActivityAskQuestion2Binding
@@ -17,21 +20,46 @@ class AskQuestion2 : AppCompatActivity() {
 
         this.binding = ActivityAskQuestion2Binding.inflate(layoutInflater)
         setContentView(binding.root)
-        val theQuestion = binding.inputTextView.text
+
+        //this.setSupportActionBar(binding.toolbar2)
 
         // Create a demonstration/proof-of-concept ShakeDetector that simply
         // displays when a shake was detected.
-        this.shakeDetector = ShakeDetector(this, this
-        ) {
-            val intent = Intent(this@AskQuestion2, ThinkingActivity ::class.java)
-            intent.putExtra("question",theQuestion)
-            startActivity(intent)
+        this.shakeDetector = ShakeDetector(this, this) {
+            val theQuestion = binding.inputTextView.text
+            val intent = Intent(this@AskQuestion2, ThinkingActivity::class.java)
+            intent.putExtra("question", theQuestion)
+
+            if (theQuestion.length > 0) {
+                startActivity(intent)
+            } else {
+                Toast.makeText(
+                    applicationContext, "you must ask a question to proceed.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            //if shake detected and question is there move to thinking activity
+            // else make toast saying "input question to continue."
         }
+    } //oncreate
 
-    //count ->
-          //Toast.makeText(this, "Shake detected ${count}", Toast.LENGTH_SHORT).show()
-//                        viewBinding.centralTextView.setText( "Detected ${count} shakes in a row" )
-       //
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.toolbarmenu, menu)
+        return true
+    } //optionsMenu
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.toolbarMenu -> {
+                val intent = Intent(this@AskQuestion2,
+                    UserAnswersActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    } // ItemSelected
+
     }
-}
-
