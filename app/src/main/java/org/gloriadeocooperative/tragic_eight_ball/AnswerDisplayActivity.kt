2007d1d70ad5883cc1bf5ba86c8edf3dec50cf67
dsh <dsh@dman13.dyndns.org>
2022-05-10 22:18:19 -0400

    Add Jedrek's latest work
    * Fixes layout in activity_answer_display
    * Displays the question passed to the activity via the Intent Extras

--- app/src/main/java/org/gloriadeocooperative/tragic_eight_ball/AnswerDisplayActivity.kt
+++ app/src/main/java/org/gloriadeocooperative/tragic_eight_ball/AnswerDisplayActivity.kt
@@ -2,10 +2,14 @@ package org.gloriadeocooperative.tragic_eight_ball
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class AnswerDisplayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_answer_display)
+        val questionDisplay= findViewById<TextView>(R.id.Question)
+        val theQuestion = intent.getStringExtra(EXTRA_QUESTION)
+        questionDisplay.setText("$theQuestion")
     }
 }
